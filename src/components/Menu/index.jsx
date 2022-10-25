6338b0263b3842e3f0e30ba6d864ff742daf58aa
Milos Naskovic <milosnaskovic17@gmail.com>
2022-10-25 10:23:52 +0200

    Created menu component with React

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  init,
+  FocusContext,
+  useFocusable,
+} from "@noriginmedia/norigin-spatial-navigation";
+
+// Components
+import MenuItem from "./MenuItem";
+
+// Styles
+import "./index.css";
+import { useEffect } from "react";
+
+init({});
+
+export default function Menu({ data, focusKey: focusKeyParam }) {
+  const { ref, focusSelf, hasFocusedChild, focusKey } = useFocusable({
+    focusable: true,
+    saveLastFocusedChild: false,
+    trackChildren: true,
+    autoRestoreFocus: true,
+    isFocusBoundary: false,
+    focusKey: focusKeyParam,
+    preferredChildFocusKey: null,
+    onEnterPress: () => {},
+    onEnterRelease: () => {},
+    onArrowPress: () => true,
+    onFocus: () => {},
+    onBlur: () => {},
+  });
+
+  useEffect(() => {
+    focusSelf();
+  }, [focusSelf]);
+
+  return (
+    <FocusContext.Provider value={focusKey}>
+      <div className="menu" ref={ref} hasFocusedChild={hasFocusedChild}>
+        {data.map((singleData) => (
+          <MenuItem key={singleData.id} {...singleData} />
+        ))}
+      </div>
+    </FocusContext.Provider>
+  );
+}
